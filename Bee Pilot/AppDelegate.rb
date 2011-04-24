89e4535964626a63434762b62fcdebce005bac06
Thomas R. Koll <tomk32@tomk32.de>
2011-04-24 08:06:05 +0200

    Initial Commit

---
+++ Bee Pilot/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  Bee Pilot
+#
+#  Created by Thomas R. Koll on 24.04.11.
+#  Copyright 2011 Ananasblau.com. All rights reserved.
+#
+
+class AppDelegate
+  attr_accessor :window
+  def applicationDidFinishLaunching(a_notification)
+    # Insert code here to initialize your application
+  end
+end
+
